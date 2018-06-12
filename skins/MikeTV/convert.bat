55fee8e4388f2a951842b2a4583f47f130c0d734
chundermike <michael_kingsley@hotmail.co.uk>
2018-06-12 22:26:11 +0100

    New skin for V1.12

---
+++ skins/MikeTV/convert.bat
@@ -0,0 +1,8 @@
+ffmpeg -i ..\..\..\Music\Videos\CountingStars.mp4 -b:v 2000000 -vf scale=320:180 ..\..\..\Music\Videos\CountingStars.ogv
+ffmpeg -i ..\..\..\Music\Videos\ForgetYou.mp4 -b:v 2000000 -vf scale=320:180 ..\..\..\Music\Videos\ForgetYou.ogv
+REM ffmpeg -i ..\..\..\Music\Videos\TakeOnMe.mp4 -b:v 2000000 -vf scale=320:180 ..\..\..\Music\Videos\TakeOnMe.ogv
+REM ffmpeg -i FeelItStill.mp4 -b:v 2000000 -vf scale=640:360 FeelItStill.ogv
+REM ffmpeg -i ..\..\..\Music\Videos\FeelItStill.mp4 -b:v 2000000 -vf scale=320:180 ..\..\..\Music\Videos\FeelItStill_small.ogv
+REM ffmpeg -i ..\..\..\Music\Videos\FeelItStill.mp4 -b:v 1000000 -vf scale=320:180 ..\..\..\Music\Videos\FeelItStill_small_lbr.ogv
+REM ffmpeg -i ..\..\..\Music\Videos\FeelItStill.mp4 -b:v 1000000 -vf scale=640:360 ..\..\..\Music\Videos\FeelItStill_lbr.ogv
+REM ffmpeg -i ..\..\..\Music\Videos\FeelItStill.mp4 -b:v 10000000 -vf scale=640:360 ..\..\..\Music\Videos\FeelItStill_hbr.ogv
